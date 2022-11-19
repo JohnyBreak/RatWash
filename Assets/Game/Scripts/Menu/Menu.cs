476568a7edb08c07f8c4af14b9f1f04a7ecad97e
JohnyBreak <rashkost@mail.ru>
2022-11-20 05:03:55 +0700

    rat list menu

--- Assets/Game/Scripts/Menu/Menu.cs
+++ Assets/Game/Scripts/Menu/Menu.cs
@@ -2,19 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using Zenject;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] private Image _joystickImg;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _canvasHolder;
+    //[SerializeField] private Image _joystickImg;
+    private Button _button;
+    private WasherStorage _washer;
+
+    [SerializeField]
+    private List<RatListElement> _ratListElements;
+
+    private void Awake()
     {
-        
+        _button = GetComponentInChildren<Button>();
+        _button.onClick.AddListener(OnButtonClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        _button.onClick.RemoveListener(OnButtonClick);
+
+        _washer.RatListChangedEvent -= UpdateElementsWhileMenuOpen;
     }
+
+    [Inject]
+    private void Constructor(WasherStorage washer)
+    {
+        _washer = washer;
+        _washer.RatListChangedEvent += UpdateElementsWhileMenuOpen;
+    }
+
+    private void SetListElements() 
+    {
+        RatListElement tempElement;
+          var dictionary = _washer.GetRatCounts();
+        foreach (var item in dictionary)
+        {
+
+            tempElement = GetElement(item.Key);
+            if (item.Value < 1)
+            {
+                tempElement.Text.gameObject.SetActive(false);
+                continue;
+            }
+            if(tempElement.Text.gameObject.activeInHierarchy == false)
+                tempElement.Text.gameObject.SetActive(true);
+
+
+            tempElement.Text.text = $"{item.Key} {item.Value}";
+        }
+    }
+
+    private RatListElement GetElement(RatSettings.RatType type) 
+    {
+        foreach (var item in _ratListElements)
+        {
+            if (item.Type == type) return item;
+        }
+        return null;
+    }
+
+    private void UpdateElementsWhileMenuOpen() 
+    {
+        if (_canvasHolder.activeInHierarchy == false) return;
+
+            SetListElements();
+    }
+
+    private void OnButtonClick() 
+    {
+        if (_canvasHolder.activeInHierarchy)
+        {
+            _canvasHolder.SetActive(false);
+
+            //_joystickImg.enabled = true;
+        }
+        else
+        {
+            SetListElements();
+            //_joystickImg.enabled = false;
+            _canvasHolder.SetActive(true);
+        }
+    }
+}
+
+[System.Serializable]
+public class RatListElement 
+{
+    public RatSettings.RatType Type;
+    public TextMeshProUGUI Text;
 }
--- Assets/Game/Scripts/Washer/WasherStorage.cs
+++ Assets/Game/Scripts/Washer/WasherStorage.cs
@@ -1,10 +1,14 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
+using System.Linq;
+using System;
 
 public class WasherStorage : MonoBehaviour
 {
+    public System.Action RatListChangedEvent;
     [SerializeField] private WasherSettings _settings;
     [SerializeField] private List<Transform> _droppersList;
     //[SerializeField] private List<WasherDropper> _droppers;
@@ -12,7 +16,7 @@ public class WasherStorage : MonoBehaviour
     private SaveManager _saveManager;
     private List<Rat> _ratList;
     private List<Rat> _ratListToWash;
-
+    private Dictionary<RatSettings.RatType, int> _ratCountDictionary;
     private float _pause;
     private int _maxActiveDropperCount;
     public WasherSettings Settings => _settings;
@@ -29,12 +33,23 @@ public class WasherStorage : MonoBehaviour
     {
         if (_maxActiveDropperCount >= _droppersList.Count)
             _maxActiveDropperCount = _droppersList.Count;
-
+        
+        _ratCountDictionary = new Dictionary<RatSettings.RatType, int>();
+        foreach (RatSettings.RatType item in Enum.GetValues(typeof(RatSettings.RatType)))
+        {
+            _ratCountDictionary.Add(item, 0);
+        }
+        //RatListChangedEvent += UpdateRatCounts;
         //_pause = _settings.Pause[_upgradeLvl];
         //_maxActiveDropperCount = _settings.MaxDropperCount[_upgradeLvl];
 
         _ratList = new List<Rat>();
         _ratListToWash = new List<Rat>();
+    }
+
+    private void OnDestroy()
+    {
+        //RatListChangedEvent -= UpdateRatCounts;
     }
 
     public void SetUpgrades(int lvl)
@@ -68,6 +83,7 @@ public class WasherStorage : MonoBehaviour
 
 
         _ratList.Clear();
+        RatListChangedEvent?.Invoke();
         if (_washRoutine != null)
         {
             StopCoroutine(_washRoutine);
@@ -103,7 +119,22 @@ public class WasherStorage : MonoBehaviour
     public void AddRat(Rat rat)
     {
         _ratList.Add(rat);
+        RatListChangedEvent?.Invoke();
         ChangeRatPosition(rat);
+    }
+
+    private void UpdateRatCounts()
+    {
+        foreach (RatSettings.RatType item in Enum.GetValues(typeof(RatSettings.RatType)))
+        {
+            _ratCountDictionary[item] = _ratList.Where(x => x.Settings.Type == item).Count();
+        }
+    }
+
+    public Dictionary<RatSettings.RatType, int> GetRatCounts() 
+    {
+        UpdateRatCounts();
+        return _ratCountDictionary;
     }
 
 }
