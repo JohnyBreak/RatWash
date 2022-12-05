f91ca074916bb74fd793351dc901f9ffe47377d9
konstantin.rashevskij <rashkost@mail.ru>
2022-12-05 17:40:24 +0700

    player stop on buy menu

--- Assets/Game/Scripts/Player/PlayerTouchMovement.cs
+++ Assets/Game/Scripts/Player/PlayerTouchMovement.cs
@@ -7,18 +7,40 @@ public class PlayerTouchMovement : MonoBehaviour
 {
     [SerializeField] private FloatingJoystick _joystick;
     [SerializeField] private NavMeshAgent _agent;
+    private BuyCanvas _buyCanvas;
     private Animator _anim;
+    private bool _canMove = true;
+
+    [Inject]
+    private void Construct(BuyCanvas buyCanvas)
+    {
+        _buyCanvas = buyCanvas;
+    }
 
     private void Awake()
     {
         _anim = GetComponentInChildren<Animator>();
+        _buyCanvas.OpenScreenEvent += OnStopPlayer;
+        _buyCanvas.HideScreenEvent += OnResumePlayer;
     }
+
+    private void OnStopPlayer() 
+    {
+        _canMove = false;
+    }
+
+    private void OnResumePlayer()
+    {
+        _canMove = true;
+    }
+
 
     private void Update()
     {
+        
 
         _anim.SetFloat("Walk",_joystick.Direction.magnitude);
-
+        if (!_canMove) return;
         if (_joystick.Direction == Vector2.zero) return;
         Vector3 movement = _agent.speed * Time.deltaTime * new Vector3(_joystick.Direction.x, 0, _joystick.Direction.y);
 
@@ -26,4 +48,10 @@ public class PlayerTouchMovement : MonoBehaviour
         _agent.Move(movement);
     }
 
+
+    private void OnDestroy()
+    {
+        _buyCanvas.OpenScreenEvent -= OnStopPlayer;
+        _buyCanvas.HideScreenEvent -= OnResumePlayer;
+    }
 }
